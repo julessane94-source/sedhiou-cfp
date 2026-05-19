205fc261fe8435d8134dd4405524c02f3f8105b5
Votre Nom <vous@exemple.com>
2026-05-19 19:46:46 +0000

    Fix: alias webpack et imports sanity/client

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,12 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["cdn.sanity.io"] },
-  swcMinify: true
+  images: { domains: ['cdn.sanity.io'] },
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 };
+
+import path from 'path';
 export default nextConfig;
