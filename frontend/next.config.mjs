1d4f1fdc24df086004f32ec04a4ca2ff32b00755
Votre Nom <vous@exemple.com>
2026-05-19 20:10:00 +0000

    Fix: Footer without SocialIcons, clean unused files

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,7 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['cdn.sanity.io'] },
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(process.cwd(), 'src');
-    return config;
-  },
 };
 
 export default nextConfig;
